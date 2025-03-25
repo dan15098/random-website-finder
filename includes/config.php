8ffbc90a54d090bb8f05dfbd271816bfb6f30d19
dan15098 <158103337+dan15098@users.noreply.github.com>
2025-03-25 15:52:34 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration (not used in this simple version)
-define('DB_HOST', 'sql306.infinityfree.com');
-define('DB_NAME', 'if0_38601620_hack');
-define('DB_USER', 'if0_38601620');
-define('DB_PASS', 'LlvVqN5uDc5Q');
+define('DB_HOST', 'the file path to reach your dg');
+define('DB_NAME', 'your sql database name');
+define('DB_USER', 'your sql db user');
+define('DB_PASS', 'your sql db password');
 
 // Maximum scan time in seconds (for shared hosting safety)
 define('MAX_SCAN_TIME', 30);
